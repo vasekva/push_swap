9c1ef83ac8b382dc372274e5bae3b4683ac1537d
vasekva <vahonin_vasek@mail.ru>
2021-10-13 00:32:28 +0300

    Have fixed a sort algorithm for four and five numbers

--- srcs/sort_five_and_four_nums.c
+++ srcs/sort_five_and_four_nums.c
@@ -31,103 +31,58 @@ int	find_put_ind(t_stack *stack, int required_number, int flag)
 			}
 			else
 			{
-				if (stack->ind != 0)
-					if (stack->past->value < required_number)
-						return (stack->ind);
+				if (stack->ind != 0 && (stack->past->value < required_number))
+					return (stack->ind);
 			}
 		}
-		if (flag == 1)
-		{
-			if (stack->next == NULL)
-				if (stack->value < required_number)
-					return (stack->ind);
-		}
+		if (flag == 1 && (stack->next == NULL
+				&& stack->value < required_number))
+			return (stack->ind);
 		stack = stack->next;
 	}
 	return (0);
 }
 
-static void	push_to_begin_or_end(t_stack **stack_a, t_stack **stack_b,
-								 int put_ind, int flag)
+static int	get_ind(t_stack *stack, int value)
 {
-	if (put_ind == -1)
+	while (stack)
 	{
-		if (get_last(*stack_a)->value < (*stack_b)->value)
-		{
-			push(stack_b, stack_a, "a", 1);
-			rotate(stack_a, "a", ft_abs(put_ind));
-			flag = 0;
-		}
-		else
-		{
-			reverse_rotate(stack_a, "a", 1);
-			if (flag == 1)
-			{
-				push(stack_b, stack_a, "a", 2);
-				rotate(stack_a, "a", 3);
-				flag = 0;
-			}
-			else
-			{
-				push(stack_b, stack_a, "a", 1);
-				rotate(stack_a, "a", 2);
-			}
-		}
+		if (stack->value == value)
+			return (stack->ind);
+		stack = stack->next;
 	}
-	else
-		push(stack_b, stack_a, "a", 1);
+	return (0);
 }
 
-static void	find_range_numbers(t_stack **stack_a, t_stack **stack_b)
+static void	push_min(t_stack **a, t_stack **b)
 {
-	t_stack	*nth;
-	int		flag;
-	int		ind;
+	int	min;
+	int	ind;
 
-	flag = 0;
-	nth = NULL;
-	while (*stack_b)
+	min = find_min_value(*a);
+	ind = get_ind(*a, min);
+	if (ind > 0)
+		rotate(a, "a", ind);
+	if (ind < 0)
 	{
-		ind = find_put_ind(*stack_a, (*stack_b)->value, 1);
-		if ((*stack_b)->next)
-		{
-			if ((*stack_b)->value > (*stack_b)->next->value)
-			{
-				nth = get_nth(*stack_a, ind);
-				if (nth->past)
-					if (nth->past->value < (*stack_b)->next->value)
-						flag = 1;
-			}
-		}
-		if (ind == -1 || ind == 0)
-		{
-			push_to_begin_or_end(stack_a, stack_b, ind, flag);
-			continue ;
-		}
-		if (ind > 0)
-			rotate(stack_a, "a", ft_abs(ind));
-		else
-			reverse_rotate(stack_a, "a", ft_abs(ind));
-		push(stack_b, stack_a, "a", 1);
-		if (ind > 0)
-			reverse_rotate(stack_a, "a", ft_abs(ind));
-		else
-			rotate(stack_a, "a", ft_abs(ind) + 1);
+		reverse_rotate(a, "a", ft_abs(ind));
 	}
+	push(a, b, "b", 1);
 }
 
 void	sort_five_and_four_nums(t_stack **a, t_stack **b)
 {
 	if (list_length(*a) == 4)
 	{
-		push(a, b, "b", 1);
+		push_min(a, b);
 		little_sort(a);
-		find_range_numbers(a, b);
+		push(b, a, "a", 1);
 	}
 	else
 	{
-		push(a, b, "b", 2);
+		push_min(a, b);
+		push_min(a, b);
 		little_sort(a);
-		find_range_numbers(a, b);
+		push(b, a, "a", 2);
 	}
 }
